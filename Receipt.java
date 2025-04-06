af6c2fa38ee30a1adc8cd3bf83de72a738e2b4ad
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-06 22:13:23 +0800

    Add files via upload

---
+++ Receipt.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Receipt {
+	public String receiptID;
+	public Applicant applicantDetails;
+	public Project projectDetails;
+	public FlatType flatType;
+	public String bookingDate;
+}
