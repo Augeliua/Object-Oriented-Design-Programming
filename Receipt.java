43646f743445c5d0aa7f58813b97d53a47a39372
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-06 22:20:11 +0800

    Delete Receipt.java

--- Receipt.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-public class Receipt {
-	public String receiptID;
-	public Applicant applicantDetails;
-	public Project projectDetails;
-	public FlatType flatType;
-	public String bookingDate;
-}
