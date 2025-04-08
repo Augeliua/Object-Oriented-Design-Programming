e56dd07b3896feaefee11ccb31444c58ab695eff
Augeliua <128767874+Augeliua@users.noreply.github.com>
2025-04-08 14:07:12 +0800

    Add files via upload

---
+++ enums/ApplicationStatus.java
@@ -0,0 +1,8 @@
+package sc2002.group.proj;
+
+public enum ApplicationStatus {
+	PENDING,
+	SUCCESSFUL,
+	UNSUCCESSFUL,
+	BOOKED
+}
---
+++ enums/EnquiryStatus.java
@@ -0,0 +1,6 @@
+package sc2002.group.proj;
+
+public enum EnquiryStatus {
+	PENDING,
+	REPLIED // ie closed
+}
