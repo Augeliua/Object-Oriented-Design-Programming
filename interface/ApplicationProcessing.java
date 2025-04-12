f7a3576f32ff662dfda9f807d64eb2227a03ca52
CALUQY <caluqy@gmail.com>
2025-04-12 22:06:27 +0800

    Delete interface/ApplicationProcessing.java

--- interface/ApplicationProcessing.java
+++
@@ -1,5 +0,0 @@
-public interface ApplicationProcessing {
-    void processApplication(String applicationDetails);
-    void validateApplication(String applicationDetails);
-    void updateApplicationStatus(String status);
-}
