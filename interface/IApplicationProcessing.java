80e754258acf37f1dfab5cc97199cdd8437bb4c0
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-13 00:16:18 +0800

    Update IApplicationProcessing.java

--- interface/IApplicationProcessing.java
+++ interface/IApplicationProcessing.java
@@ -1,3 +1,7 @@
+package sc2002.bto.interfaces;
+
+import sc2002.bto.entity.Application;
+
 public interface IApplicationProcessing {
     void processApplication(Application a);
     void validateApplication(Application a);
