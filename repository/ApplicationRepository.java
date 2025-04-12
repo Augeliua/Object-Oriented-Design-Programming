4a048348dd796faba79a6c658b1fe78991aa6fe3
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-13 00:17:50 +0800

    Update ApplicationRepository.java

--- repository/ApplicationRepository.java
+++ repository/ApplicationRepository.java
@@ -1,28 +1,53 @@
+package sc2002.bto.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import sc2002.bto.entity.Applicant;
+import sc2002.bto.entity.Application;
+import sc2002.bto.interfaces.IRepository;
+
 public class ApplicationRepository implements IRepository<Application> {
+    private List<Application> applications = new ArrayList<>();
+    
     @Override
     public Application getById(String id) {
-        // Implementation details
-        return null;
+        return applications.stream()
+                .filter(app -> app.getApplicationId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
     
     @Override
     public List<Application> getAll() {
-        // Implementation details
-        return null;
+        return new ArrayList<>(applications);
     }
     
     @Override
     public void add(Application item) {
-        // Implementation details
+        applications.add(item);
     }
     
     @Override
     public void update(Application item) {
-        // Implementation details
+        for (int i = 0; i < applications.size(); i++) {
+            if (applications.get(i).getApplicationId().equals(item.getApplicationId())) {
+                applications.set(i, item);
+                return;
+            }
+        }
+    }
+    
+    @Override
+    public void delete(String id) {
+        applications.removeIf(app -> app.getApplicationId().equals(id));
     }
     
     public List<Application> findByApplicant(Applicant applicant) {
-        // Implementation details
-        return null;
+        return applications.stream()
+                .filter(app -> app.getApplicant() != null && 
+                         app.getApplicant().getId().equals(applicant.getId()))
+                .collect(Collectors.toList());
     }
 }
