7bef8b6c55d85c553a911a5da06d0a93d155b661
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-13 00:18:57 +0800

    Update UserRepository.java

--- repository/UserRepository.java
+++ repository/UserRepository.java
@@ -1,28 +1,52 @@
+package sc2002.bto.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import sc2002.bto.entity.User;
+import sc2002.bto.interfaces.IRepository;
+
 public class UserRepository implements IRepository<User> {
+    private List<User> users = new ArrayList<>();
+    
     @Override
     public User getById(String id) {
-        // Implementation details
-        return null;
+        return users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
     
     @Override
     public List<User> getAll() {
-        // Implementation details
-        return null;
+        return new ArrayList<>(users);
     }
     
     @Override
     public void add(User item) {
-        // Implementation details
+        users.add(item);
     }
     
     @Override
     public void update(User item) {
-        // Implementation details
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId().equals(item.getId())) {
+                users.set(i, item);
+                return;
+            }
+        }
+    }
+    
+    @Override
+    public void delete(String id) {
+        users.removeIf(u -> u.getId().equals(id));
     }
     
     public User findByUsername(String username) {
-        // Implementation details
-        return null;
+        return users.stream()
+                .filter(u -> u.getName().equals(username))
+                .findFirst()
+                .orElse(null);
     }
 }
