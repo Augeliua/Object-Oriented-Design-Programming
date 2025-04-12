8f413eebae567d87f8d091946242b21849149c48
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-13 00:14:21 +0800

    Update IRepository.java

--- interface/IRepository.java
+++ interface/IRepository.java
@@ -1,6 +1,11 @@
+package sc2002.bto.interfaces;
+
+import java.util.List;
+
 public interface IRepository<T> {
     T getById(String id);
     List<T> getAll();
     void add(T item);
     void update(T item);
+    void delete(String id);
 }
