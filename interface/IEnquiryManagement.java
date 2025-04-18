9233c6e3fa5cd14e0196b9b7b0a93a3e251a66c7
Augeliua <128767874+Augeliua@users.noreply.github.com>
2025-04-18 13:50:06 +0800

    Delete interface directory

--- interface/IEnquiryManagement.java
+++
@@ -1,8 +0,0 @@
-package sc2002.bto.interfaces;
-
-import sc2002.bto.entity.Enquiry;
-
-public interface IEnquiryManagement {
-    void handleEnquiry(Enquiry e);
-    void respondToEnquiry(Enquiry e, String r);
-}
--- interface/IProjectManagement.java
+++
@@ -1,8 +0,0 @@
-package sc2002.bto.interfaces;
-
-import sc2002.bto.entity.Project;
-
-public interface IProjectManagement {
-    void reviewProject(Project p);
-    void approveProject(Project p);
-}
