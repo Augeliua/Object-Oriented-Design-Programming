b9d2feab28615569a5d3a9fdc58b2b1256b2e8d6
Augeliua <128767874+Augeliua@users.noreply.github.com>
2025-04-18 13:59:35 +0800

    Create UserRepository.java

---
+++ sc2002/bto/repository/UserRepository.java
@@ -0,0 +1,51 @@
+package sc2002.bto.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sc2002.bto.entity.User;
+import sc2002.bto.interfaces.IRepository;
+
+public class UserRepository implements IRepository<User> {
+    private List<User> users = new ArrayList<>();
+    
+    @Override
+    public User getById(String id) {
+        return users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+    
+    @Override
+    public List<User> getAll() {
+        return new ArrayList<>(users);
+    }
+    
+    @Override
+    public void add(User item) {
+        users.add(item);
+    }
+    
+    @Override
+    public void update(User item) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId().equals(item.getId())) {
+                users.set(i, item);
+                return;
+            }
+        }
+    }
+    
+    @Override
+    public void delete(String id) {
+        users.removeIf(u -> u.getId().equals(id));
+    }
+    
+    public User findByUsername(String username) {
+        return users.stream()
+                .filter(u -> u.getName().equals(username))
+                .findFirst()
+                .orElse(null);
+    }
+}
