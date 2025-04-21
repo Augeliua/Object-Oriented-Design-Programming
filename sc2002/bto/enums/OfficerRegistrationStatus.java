c4572f486ee15e08e7300358f490c6a4a58c9f6b
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:34:39 +0800

    Update OfficerRegistrationStatus.java

--- sc2002/bto/enums/OfficerRegistrationStatus.java
+++ sc2002/bto/enums/OfficerRegistrationStatus.java
@@ -1,7 +1,19 @@
 package sc2002.bto.enums;
-
+/**
+ * Represents the status of an HDB Officer's registration to handle a project.
+ * 
+ */
 public enum OfficerRegistrationStatus {
+    /**
+     * Registration request has been submitted but not yet approved or rejected
+     */
     PENDING,
+    /**
+     * Registration request has been approved by the HDB Manager
+     */
     APPROVED,
+    /**
+     * Registration request has been rejected by the HDB Manager
+     */
     REJECTED
 }
