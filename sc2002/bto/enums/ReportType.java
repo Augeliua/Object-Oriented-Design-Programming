072e98e14bb4b62c63d156724272182c1544340c
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:40:18 +0800

    Update ReportType.java

--- sc2002/bto/enums/ReportType.java
+++ sc2002/bto/enums/ReportType.java
@@ -1,7 +1,19 @@
 package sc2002.bto.enums;
-
+/**
+ * Represents the types of reports that can be generated in the BTO system.
+ * 
+ */
 public enum ReportType {
+    /**
+     * Report showing all flat bookings across projects
+     */
     ALL_BOOKINGS,
+    /**
+     * Report of bookings categorized by flat type
+     */
     BY_FLAT_TYPE,
+    /**
+     * Report of bookings categorized by applicants' marital status
+     */
     BY_MARITAL_STATUS
 }
