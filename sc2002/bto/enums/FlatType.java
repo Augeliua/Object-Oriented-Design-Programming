73001d4d6fa93a56271252880dbf8790a9279bdc
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:34:01 +0800

    Update FlatType.java

--- sc2002/bto/enums/FlatType.java
+++ sc2002/bto/enums/FlatType.java
@@ -1,6 +1,15 @@
 package sc2002.bto.enums;
-
+/**
+ * Represents the types of flats available in BTO projects.
+ * 
+ */
 public enum FlatType {
+    /**
+     * Two-room flat type - available to all eligible applicants
+     */
     TWO_ROOM,
+    /**
+     * Three-room flat type - only available to married applicants
+     */
     THREE_ROOM
 }
