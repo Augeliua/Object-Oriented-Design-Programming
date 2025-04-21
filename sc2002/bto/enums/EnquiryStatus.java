0cb72d9e9e14a8515da6954c8b6cb57fdc2880c0
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:33:46 +0800

    Update EnquiryStatus.java

--- sc2002/bto/enums/EnquiryStatus.java
+++ sc2002/bto/enums/EnquiryStatus.java
@@ -1,6 +1,15 @@
 package sc2002.bto.enums;
-
+/**
+ * Represents the possible statuses of an enquiry in the BTO system.
+ * 
+ */
 public enum EnquiryStatus {
+    /**
+     * Enquiry has been submitted but not yet answered
+     */
     PENDING,
+    /**
+     * Enquiry has been answered by an HDB Officer or Manager
+     */
     REPLIED
 }
