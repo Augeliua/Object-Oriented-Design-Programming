d57ab0e43d8cc167d6b07d48d60681eb2ec1b1f2
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:41:42 +0800

    Update IProjectManagement.java

--- sc2002/bto/interfaces/IProjectManagement.java
+++ sc2002/bto/interfaces/IProjectManagement.java
@@ -1,8 +1,22 @@
 package sc2002.bto.interfaces;
 
 import sc2002.bto.entity.Project;
-
+/**
+ * Defines operations for managing BTO projects.
+ * This interface is implemented by classes responsible for reviewing and approving projects.
+ * 
+ */
 public interface IProjectManagement {
+    /**
+     * Reviews the details of a project.
+     * 
+     * @param p The project to review
+     */
     void reviewProject(Project p);
+    /**
+     * Approves a project, making it officially available.
+     * 
+     * @param p The project to approve
+     */
     void approveProject(Project p);
 }
