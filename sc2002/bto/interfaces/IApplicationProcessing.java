cc93cac3cea02b18a732e6603fc9a2c66ada8071
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:40:36 +0800

    Update IApplicationProcessing.java

--- sc2002/bto/interfaces/IApplicationProcessing.java
+++ sc2002/bto/interfaces/IApplicationProcessing.java
@@ -1,9 +1,26 @@
 package sc2002.bto.interfaces;
 
 import sc2002.bto.entity.Application;
-
+/**
+ * Defines operations for processing BTO applications.
+ * This interface is implemented by classes responsible for validating and updating applications.
+ * 
+ */
 public interface IApplicationProcessing {
+    /**
+     * Processes an application by handling the appropriate actions based on its status.
+     * 
+     * @param a The application to process
+     */
     void processApplication(Application a);
+    /**
+     * Validates an application to ensure all criteria are met.
+     * 
+     * @param a The application to validate
+     */
     void validateApplication(Application a);
+    /**
+     * Updates the status of application(s) based on current conditions.
+     */
     void updateApplicationStatus();
 }
