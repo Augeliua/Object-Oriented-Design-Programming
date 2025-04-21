555837c61b566b691f4edfb318e148fb56d35870
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:34:23 +0800

    Update MaritalStatus.java

--- sc2002/bto/enums/MaritalStatus.java
+++ sc2002/bto/enums/MaritalStatus.java
@@ -1,6 +1,16 @@
 package sc2002.bto.enums;
-
+/**
+ * Represents the marital status of a user in the BTO system.
+ * Marital status affects the types of flats an applicant is eligible for.
+ * 
+ */
 public enum MaritalStatus {
+    /**
+     * Single status - applicants must be at least 35 years old and can only apply for 2-Room flats
+     */
     SINGLE,
+    /**
+     * Married status - applicants must be at least 21 years old and can apply for any flat type
+     */
     MARRIED
 }
