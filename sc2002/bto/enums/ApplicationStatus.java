30b6e3330bb54485c5fa3fd1250973f99b1a3592
aaronervepin <48801092+aaronervepin@users.noreply.github.com>
2025-04-21 12:33:06 +0800

    Update ApplicationStatus.java

--- sc2002/bto/enums/ApplicationStatus.java
+++ sc2002/bto/enums/ApplicationStatus.java
@@ -1,9 +1,26 @@
 package sc2002.bto.enums;
-
+/**
+ * Represents the possible statuses of a BTO application.
+ * These statuses track the application through its lifecycle from
+ * submission to completion.
+ * 
+ */
 public enum ApplicationStatus {
+    /**
+     * Initial status upon application submission - no decision made yet
+     */
     PENDING,
+    /**
+     * Application has been approved - applicant is invited to book a flat
+     */
     SUCCESSFUL,
+    /**
+     * Application has been rejected - applicant cannot proceed to booking
+     */
     UNSUCCESSFUL,
+     /**
+     * Applicant has successfully booked a flat
+     */
     BOOKED
 }
 
